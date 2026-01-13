4042f7da5c921eca428a294f498bd780b20b6360
felixalbert97 <felixalbert97@web.de>
2026-01-13 14:49:05 +0100

    Cross origin requests allowed

--- src/main/java/de/felixalbert/expense_tracker/expense/ExpenseController.java
+++ src/main/java/de/felixalbert/expense_tracker/expense/ExpenseController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/expenses")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ExpenseController {
 
     private final ExpenseRepository expenseRepository;
