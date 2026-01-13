1065e005482bfa709fc62e67483b07b76496967e
felixalbert97 <felixalbert97@web.de>
2026-01-13 12:45:24 +0100

    Add GET endpoint for expenses

---
+++ src/main/java/de/felixalbert/expense_tracker/expense/ExpenseController.java
@@ -0,0 +1,23 @@
+package de.felixalbert.expense_tracker.expense;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/expenses")
+public class ExpenseController {
+
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseController(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    @GetMapping
+    public List<Expense> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+}
