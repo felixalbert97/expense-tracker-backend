0718f7717676645707df1f9cab75217cd0328b15
felixalbert97 <felixalbert97@web.de>
2026-01-13 11:18:26 +0100

    Initial Spring Boot project setup

---
+++ src/test/java/de/felixalbert/expense_tracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.felixalbert.expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
