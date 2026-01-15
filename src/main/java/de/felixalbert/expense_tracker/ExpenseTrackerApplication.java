c70fcd95ab1d2d42cf28267c9a6f6a8adf4cde3b
felixalbert97 <felixalbert97@web.de>
2026-01-15 02:08:20 +0100

    Added Cors Configurer to allow frontend access

--- src/main/java/de/felixalbert/expense_tracker/ExpenseTrackerApplication.java
+++ src/main/java/de/felixalbert/expense_tracker/ExpenseTrackerApplication.java
@@ -3,6 +3,12 @@ package de.felixalbert.expense_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
@@ -10,4 +16,17 @@ public class ExpenseTrackerApplication {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // alle Endpoints
+						.allowedOrigins("https://expense-tracker-frontend-felixalbert97s-projects.vercel.app/") // Frontend URL
+						.allowedMethods("*"); // GET, POST, PUT, DELETE
+			}
+		};
+	}
 }
+
+
